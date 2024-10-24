17a2b94b2f82efd7d79efb7a33f00108cbd72aeb
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-24 20:33:05 +0700

    delete user button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Dedi Setiawan</h2>
       <Button>Subscribe</Button>
-      <UserButton />
+      
     </div>
   );
 }
