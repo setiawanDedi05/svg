722bdcf7b1b6749af15e8bd00ae62e6493854e79
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-24 17:48:00 +0700

    add api get video script

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/clerk/webhook(.*)",
+  "/api/get-video-script(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
